c3c8265fbc8f3834f5fe2868702317a0b4581771
Sreerag Haridas EM <61047208+sreeragdas@users.noreply.github.com>
2025-12-31 10:00:22 +0530

    Time: 26 ms (11.71%) | Memory: 63.1 MB (5.14%) - LeetSync

--- 268-missing-number/missing-number.js
+++ 268-missing-number/missing-number.js
@@ -3,8 +3,7 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
-//sortNum[i+1] =index
-//sortNum[i] +1 =array value 
+
 let sortNum = [...new Set(nums)].sort((a,b) => a - b);
 
 for (let i = 0; i < sortNum.length; i++) {
